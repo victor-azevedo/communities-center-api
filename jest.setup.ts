e6e9b8f45d8adcfa1f15e59317e259213a213dd6
Victor Azevedo <josevcazevedo@gmail.com>
2024-08-17 22:21:56 -0300

    test: add jest initial config

---
+++ jest.setup.ts
@@ -0,0 +1,18 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongoServer: MongoMemoryServer;
+
+// Configuração do Jest para aplicar em todos os testes, evita a necessidade de repetir o código
+beforeAll(async () => {
+  // Cria instancia em memória do MongoDB para realizar os testes, evita a necessidade de ter um banco de dados real
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+afterAll(async () => {
+  // Fecha conexão com o MongoDB ao final dos testes
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
