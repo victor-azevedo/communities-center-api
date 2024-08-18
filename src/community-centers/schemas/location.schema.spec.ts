d4becccf2043280de689142830f882ee464573ee
Victor Azevedo <josevcazevedo@gmail.com>
2024-08-17 22:23:49 -0300

    feat: add location schema

---
+++ src/community-centers/schemas/location.schema.spec.ts
@@ -0,0 +1,54 @@
+import {
+  validateLatitude,
+  validateLatitudeErrorMessage,
+  validateLongitude,
+  validateLongitudeErrorMessage,
+} from './location.schema';
+
+describe('[LocationModel] validateLatitude', () => {
+  it('should return true for a valid latitude within the range -90 to 90', () => {
+    const validLatitudes = [-90, 0, 45.123, 90];
+
+    validLatitudes.forEach((lat) => {
+      const result = validateLatitude.validator(lat);
+      expect(result).toBe(true);
+    });
+  });
+
+  it('should return false for an invalid latitude outside the range -90 to 90', () => {
+    const invalidLatitudes = [-91, 90.1, 100, -100];
+
+    invalidLatitudes.forEach((lat) => {
+      const result = validateLatitude.validator(lat);
+      expect(result).toBe(false);
+    });
+  });
+
+  it('should return the correct error message for invalid quantity', () => {
+    expect(validateLatitude.message).toBe(validateLatitudeErrorMessage);
+  });
+});
+
+describe('validateLongitude', () => {
+  it('should return true for a valid longitude within the range -180 to 180', () => {
+    const validLongitudes = [-180, 0, 78.456, 180];
+
+    validLongitudes.forEach((long) => {
+      const result = validateLongitude.validator(long);
+      expect(result).toBe(true);
+    });
+  });
+
+  it('should return false for an invalid longitude outside the range -180 to 180', () => {
+    const invalidLongitudes = [-181, 180.1, 200, -200];
+
+    invalidLongitudes.forEach((long) => {
+      const result = validateLongitude.validator(long);
+      expect(result).toBe(false);
+    });
+  });
+
+  it('should return the correct error message for invalid quantity', () => {
+    expect(validateLongitude.message).toBe(validateLongitudeErrorMessage);
+  });
+});
---
+++ src/community-centers/schemas/location.schema.ts
@@ -0,0 +1,36 @@
+import { Schema } from 'mongoose';
+import { SchemaValidateType } from '../../types/schema-validate.type';
+
+export type LocationType = {
+  latitude: number;
+  longitude: number;
+};
+
+export const validateLatitudeErrorMessage =
+  'Latitude must be between -90 and 90 degrees';
+export const validateLatitude: SchemaValidateType = {
+  validator: (lat: number) => lat >= -90 && lat <= 90,
+  message: validateLatitudeErrorMessage,
+};
+
+export const validateLongitudeErrorMessage =
+  'Longitude must be between -180 and 180 degrees';
+export const validateLongitude: SchemaValidateType = {
+  validator: (long: number) => long >= -180 && long <= 180,
+  message: validateLongitudeErrorMessage,
+};
+
+const LocationSchema = new Schema<LocationType>({
+  latitude: {
+    type: Number,
+    required: true,
+    validate: validateLatitude,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+    validate: validateLongitude,
+  },
+});
+
+export { LocationSchema };
