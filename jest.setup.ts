887aa0f0294b5cf64952167fbffa17af1b958477
Victor Azevedo <josevcazevedo@gmail.com>
2024-08-19 11:42:54 -0300

    chore: Update MongoDB connection URL in .env.example and jest.setup.ts to replicaSet

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,20 +1,22 @@
-import { mongoConfig } from './src/database/mongo.config';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MongoMemoryReplSet } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import { mongoConfig } from './src/database/mongo.config';
 
-let mongoServer: MongoMemoryServer;
+let mongoServer: MongoMemoryReplSet;
 
 // Configuração do Jest para aplicar em todos os testes, evita a necessidade de repetir o código
 beforeAll(async () => {
   // Cria instancia em memória do MongoDB para realizar os testes, evita a necessidade de ter um banco de dados real
-  mongoServer = await MongoMemoryServer.create();
+  const mongoServer = await MongoMemoryReplSet.create({
+    replSet: { count: 4 },
+  });
   const mongoUri = mongoServer.getUri();
 
   await mongoConfig(mongoUri);
 });
 
 afterAll(async () => {
-  // Fecha conexão com o MongoDB ao final dos testes
+  // // Fecha conexão com o MongoDB ao final dos testes
   await mongoose.disconnect();
-  await mongoServer.stop();
+  await mongoose.connection.close();
 });
