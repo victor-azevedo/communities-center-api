96b4b052cb70a510afed60747994330a264a87ba
Victor Azevedo <josevcazevedo@gmail.com>
2024-08-19 00:39:37 -0300

    feat: Refactor ApplicationError to include details parameter

--- src/errors/application-error.ts
+++ src/errors/application-error.ts
@@ -1,17 +1,24 @@
 export class ApplicationError extends Error {
   private _statusCode: number;
+  private _details: string[];
 
-  constructor({ message, statusCode }: ApplicationErrorParams) {
+  constructor({ message, statusCode, details }: ApplicationErrorParams) {
     super(message);
     this._statusCode = statusCode;
+    this._details = details || [];
   }
 
   get statusCode(): number {
     return this._statusCode;
+  }
+
+  get details(): string[] {
+    return this._details;
   }
 }
 
 export interface ApplicationErrorParams {
   message: string;
   statusCode: number;
+  details?: string[];
 }
--- src/errors/bad-request-error.ts
+++ src/errors/bad-request-error.ts
@@ -2,7 +2,13 @@ import httpStatus from 'http-status';
 import { ApplicationError } from './application-error';
 
 export class BadRequestError extends ApplicationError {
-  constructor(message = 'Bad Request') {
-    super({ message, statusCode: httpStatus.BAD_REQUEST });
+  constructor({
+    message = 'Bad Request',
+    details,
+  }: {
+    message?: string;
+    details?: string[];
+  }) {
+    super({ message, statusCode: httpStatus.BAD_REQUEST, details });
   }
 }
